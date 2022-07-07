7f04a695e6868236bda6976b2c68ee671aaa7d95
Mariana Bastos <maribastossrodrigues@gmail.com>
2022-07-06 21:38:37 -0300

    error in unit tests

--- dev-training/src/courses/courses.service.spec.ts
+++ dev-training/src/courses/courses.service.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Connection, Repository } from 'typeorm';
 import { CoursesService } from './courses.service';
+import { Course } from './entities/course.entity';
+import { Tag } from './entities/tag.entity';
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+});
 
 describe('CoursesService', () => {
   let service: CoursesService;
+  let courseRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoursesService],
+      providers: [
+        CoursesService,
+        { provide: Connection, useValue: {} },
+        {
+          provide: getRepositoryToken(Course),
+          useValue: createMockRepository(),
+        },
+        { provide: getRepositoryToken(Tag), useValue: createMockRepository()},
+      ],
     }).compile();
 
     service = module.get<CoursesService>(CoursesService);
+    courseRepository = module.get<MockRepository>(getRepositoryToken(Course));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should return course by id', async () => {
+      const courseId = '1';
+      const expectedCourse = {};
+
+      courseRepository.findOne.mockReturnValue(expectedCourse)
+      const course = await service.findOne(courseId);
+      expect(course).toEqual(expectedCourse);
+    });
+
+    it('should return an not found exception if course id not exists', () => {
+
+    });
   });
 });
